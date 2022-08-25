69500271b6427e56c24f8b756b6c1c5d33a9d718
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-08-25 12:24:20 +0500

    onzone-app

--- src/pages/Admin/TypeCategory.jsx
+++ src/pages/Admin/TypeCategory.jsx
@@ -14,31 +14,54 @@ const TypeCategory = (props) => {
     // const [category, setCategory] = useState([])
     const dispatch = useDispatch()
 
-    
-const config = {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "multipart/form-data",
-    },
-  };
 
-    const createCategory = async e => {
-        e.preventDefault()
+    const config = {
+        headers: {
+            // 'Content-Type': 'multipart/form-data'
+            "Content-Type": 'application/json'
+        },
+    };
 
-        const formData = new FormData()
-        formData.append('image', file)
+    // const createCategory = async e => {
+    //     e.preventDefault()
 
-        await axios.post(API_PATH + '/admins/create-type-category/', {
-            "image": file,
-            "translations": {
-                "uz": {
-                    "name": name
-                },
-                "ru": {
-                    "name": nameRu
-                }
-            }
-        }, config)
+    //     let formData = new FormData()
+    //     formData.append('image', file)
+    //     formData.append("translations", {
+    //         "uz": {
+    //             "name": `${name}`
+    //         },
+    //         "ru": {
+    //             "name": `${nameRu}`
+    //         }
+    //     })
+
+
+
+
+    //     console.log(formData);
+
+    //     await axios.post(API_PATH + '/admins/create-type-category/', formData, config)
+    // }
+
+    const postCategory =  (e) => {
+        e.preventDefault();
+        let uz = { name: name }
+        let ru = { name: nameRu }
+        const formData = {};
+        // formData.image = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
+        formData.translations = {uz, ru}
+         // let translations = {uz, ru}
+        // formData.append("image", "https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/homepage/families-gallery/2022/04_12/family_chooser_tecnica_m.png")
+        // formData.append("translations", translations)
+        console.log(JSON.stringify(formData));
+        axios.post(API_PATH + '/admins/create-type-category/', JSON.stringify(formData), config)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     useEffect(() => {
@@ -52,14 +75,14 @@ const config = {
             <div className="TypeCategory">
                 <div className="container">
                     <div className="row">
-                        <form onSubmit={createCategory} className="cards col-lg-4">
+                        <form onSubmit={postCategory} className="cards col-lg-4">
                             <h5 className='mb-5'>Type category</h5>
                             <input required onChange={e => setName(e.target.value)} value={name} placeholder='Name uz...' type="text" className="form-control" />
                             <input required onChange={e => setNameRu(e.target.value)} value={nameRu} placeholder='Name ru...' type="text" className="form-control my-4" />
-                            <input required onChange={e => setFile(e.target.files[0])} type="file" className="form-control my-4" />
-                            {file &&
+                            {/* <input required onChange={e => setFile(e.target.value)} type="text" className="form-control my-4" /> */}
+                            {/* {file &&
                                 <div className='img'><img src={URL.createObjectURL(file)} alt="" /></div>
-                            }
+                            } */}
                             <button className='btn btn-dark ms-auto d-block' type="submit">Enter</button>
                         </form>
                     </div>
