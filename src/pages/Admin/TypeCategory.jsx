5da7dc13ca8935dfbb9d92c0cb9d33d750074ed5
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-08-26 11:20:35 +0500

    onzone-app

--- src/pages/Admin/TypeCategory.jsx
+++ src/pages/Admin/TypeCategory.jsx
@@ -14,48 +14,22 @@ const TypeCategory = (props) => {
     // const [category, setCategory] = useState([])
     const dispatch = useDispatch()
 
-
     const config = {
         headers: {
-            // 'Content-Type': 'multipart/form-data'
-            "Content-Type": 'application/json'
+            Accept: "application/json",
+            "Content-Type": "multipart/form-data",
         },
     };
 
-    // const createCategory = async e => {
-    //     e.preventDefault()
-
-    //     let formData = new FormData()
-    //     formData.append('image', file)
-    //     formData.append("translations", {
-    //         "uz": {
-    //             "name": `${name}`
-    //         },
-    //         "ru": {
-    //             "name": `${nameRu}`
-    //         }
-    //     })
-
-
-
-
-    //     console.log(formData);
-
-    //     await axios.post(API_PATH + '/admins/create-type-category/', formData, config)
-    // }
-
-    const postCategory =  (e) => {
+    const postCategory = async (e) => {
         e.preventDefault();
-        let uz = { name: name }
-        let ru = { name: nameRu }
-        const formData = {};
-        // formData.image = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
-        formData.translations = {uz, ru}
-         // let translations = {uz, ru}
-        // formData.append("image", "https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/homepage/families-gallery/2022/04_12/family_chooser_tecnica_m.png")
-        // formData.append("translations", translations)
-        console.log(JSON.stringify(formData));
-        axios.post(API_PATH + '/admins/create-type-category/', JSON.stringify(formData), config)
+
+        const formData = new FormData()
+        formData.append('image', file)
+        formData.append('name', name)
+        formData.append('name_ru', nameRu)
+
+        await axios.post(API_PATH + '/admins/create-type-category/', formData, config)
             .then(res => {
                 console.log(res);
             })
@@ -79,10 +53,10 @@ const TypeCategory = (props) => {
                             <h5 className='mb-5'>Type category</h5>
                             <input required onChange={e => setName(e.target.value)} value={name} placeholder='Name uz...' type="text" className="form-control" />
                             <input required onChange={e => setNameRu(e.target.value)} value={nameRu} placeholder='Name ru...' type="text" className="form-control my-4" />
-                            {/* <input required onChange={e => setFile(e.target.value)} type="text" className="form-control my-4" /> */}  
-                            {/* {file &&
+                            <input required onChange={e => setFile(e.target.files[0])} type="file" className="form-control my-4" />
+                            {file &&
                                 <div className='img'><img src={URL.createObjectURL(file)} alt="" /></div>
-                            } */}
+                            }
                             <button className='btn btn-dark ms-auto d-block' type="submit">Enter</button>
                         </form>
                     </div>
@@ -90,14 +64,14 @@ const TypeCategory = (props) => {
 
                 <div className="container py-5">
                     <div className="row">
-                        {/* {props.categories?.map((item, index) => (
+                        {props.categories?.map((item, index) => (
                             <div key={index} className="col-lg-3">
                                 <div className="img">
                                     <img className='w-100' src={item.image} alt="" />
                                 </div>
                                 <h5>{item.name}</h5>
                             </div>
-                        ))} */}
+                        ))}
                     </div>
                 </div>
             </div>
--- src/redux/actions/adminActions.js
+++ src/redux/actions/adminActions.js
@@ -20,8 +20,8 @@ export const getCategory = () => async (dispatch) => {
   await axios
     .get(API_PATH + "/product/list-type-category/")
     .then((res) => {
-    //   console.log(res);
-      dispatch(updateAdmin({categories: res.data.results}))
+      //   console.log(res);
+      dispatch(updateAdmin({ categories: res.data.results }));
       //   setCategory(res.data.results);
     })
     .catch((err) => {
@@ -29,22 +29,37 @@ export const getCategory = () => async (dispatch) => {
     });
 };
 
+// export const postCategory = (e, name, nameRu, file) => async (dispatch) => {
+//   e.preventDefault();
 
+//   const formData = new FormData();
+//   formData.append("image", file);
+//   formData.append("name", name);
+//   formData.append("name_ru", nameRu);
 
+//   await axios
+//     .post(API_PATH + "/admins/create-type-category/", formData, config)
+//     .then((res) => {
+//       console.log(res);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
 
-export const createCategory = (e, name, file) => async (dispatch) => {
-  e.preventDefault();
+// export const createCategory = (e, name, file) => async (dispatch) => {
+//   e.preventDefault();
 
-  const formData = new FormData();
-  formData.append("name", name);
-  formData.append("image", file);
-  await axios
-    .post(API_PATH + "/admins/create-type-category/", formData, config)
-    .then((res) => {
-    //   console.log(res);
-      //   getCategry();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+//   const formData = new FormData();
+//   formData.append("name", name);
+//   formData.append("image", file);
+//   await axios
+//     .post(API_PATH + "/admins/create-type-category/", formData, config)
+//     .then((res) => {
+//       //   console.log(res);
+//       //   getCategry();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
