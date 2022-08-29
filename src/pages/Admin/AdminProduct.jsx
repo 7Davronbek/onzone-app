53489d97245243515e7474d8ba2a7fb03d3edd50
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-08-29 10:11:54 +0500

    onzone-app

--- src/pages/Admin/AdminProduct.jsx
+++ src/pages/Admin/AdminProduct.jsx
@@ -3,23 +3,29 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { connect, useDispatch } from 'react-redux'
 import AdminLayout from '../../components/AdminLayout'
-import { getAttributes, deleteAttribute } from '../../redux/actions/adminActions'
+import { getAttributes, getBrands, getSubCategories, deleteAttribute } from '../../redux/actions/adminActions'
 import { API_PATH } from '../../tools/constants'
 
 const AdminProduct = (props) => {
     const [name, setName] = useState('')
     const [nameRu, setNameRu] = useState('')
+    const [brand, setBrand] = useState('')
+    const [category, setCategory] = useState('')
+    const [attribute, setAttribute] = useState('')
 
     const dispatch = useDispatch()
 
     const createBrand = async e => {
         e.preventDefault()
 
-        await axios.post(API_PATH + `/admins/create-attribute/`, { name, name_ru: nameRu })
+        await axios.post(API_PATH + `/admins/create-product/`, { name, name_ru: nameRu, brand: Number(brand), category: Number(category), attribute: Number(attribute) })
             .then(res => {
-                dispatch(getAttributes())
+                // dispatch(getAttributes())
                 setName('')
                 setNameRu('')
+                setBrand('')
+                setCategory('')
+                setAttribute('')
             })
             .catch(err => {
                 console.log(err);
@@ -27,6 +33,8 @@ const AdminProduct = (props) => {
     }
 
     useEffect(() => {
+        props.getBrands()
+        props.getSubCategories()
         props.getAttributes()
     }, [])
 
@@ -39,22 +47,43 @@ const AdminProduct = (props) => {
                             <h5>Products</h5>
                         </div>
                         <form onSubmit={createBrand} className="col-lg-4">
-                            <input onChange={e => setName(e.target.value)} value={name} className='form-control mb-3' type="text" placeholder='Name uz...' />
-                            <input onChange={e => setNameRu(e.target.value)} value={nameRu} className='form-control mb-3' type="text" placeholder='Name ru...' />
+                            <input required onChange={e => setName(e.target.value)} value={name} className='form-control mb-3' type="text" placeholder='Name uz...' />
+                            <input required onChange={e => setNameRu(e.target.value)} value={nameRu} className='form-control mb-3' type="text" placeholder='Name ru...' />
+
+                            <select onChange={e => setBrand(e.target.value)} className='form-control mb-3'>
+                                <option className='form-control' value="0">Choose brand</option>
+                                {props.brands && props.brands.map((item, index) => (
+                                    <option key={item.id} className='form-control' value={item.id}>{item.name}</option>
+                                ))}
+                            </select>
+
+                            <select onChange={e => setCategory(e.target.value)} className='form-control mb-3'>
+                                <option className='form-control' value="0">Choose category</option>
+                                {props.subCategories && props.subCategories.map((item, index) => (
+                                    <option key={item.id} className='form-control' value={item.id}>{item.name}</option>
+                                ))}
+                            </select>
+
+                            <select onChange={e => setAttribute(e.target.value)} className='form-control mb-3'>
+                                <option className='form-control' value="0">Choose attribute</option>
+                                {props.attributes && props.attributes.map((item, index) => (
+                                    <option key={item.id} className='form-control' value={item.id}>{item.name}</option>
+                                ))}
+                            </select>
 
                             <button type='submit' className="btn btn-dark">Send</button>
                         </form>
                     </div>
 
                     <div className="row mt-5">
-                        {props.attributes && props.attributes.map((item, index) => (
+                        {/* {props.attributes && props.attributes.map((item, index) => (
                             <div key={index} className="col-lg-3 mb-4 h-100 shadow p-3">
                                 <h5>{item.name}</h5>
                                 <div className="d-flex align-items-center justify-content-end mt-3">
                                     <button onClick={() => props.deleteAttribute(item.id)} className="btn"><img src="/image/icon/delete.svg" alt="" /></button>
                                 </div>
                             </div>
-                        ))}
+                        ))} */}
                     </div>
                 </div>
             </div>
@@ -64,8 +93,10 @@ const AdminProduct = (props) => {
 
 const mapStateToProps = state => {
     return {
+        brands: state.admin.brands,
+        subCategories: state.admin.subCategories,
         attributes: state.admin.attributes,
     }
 }
 
-export default connect(mapStateToProps, { getAttributes, deleteAttribute })(AdminProduct)  
+export default connect(mapStateToProps, { getAttributes, getBrands, getSubCategories, deleteAttribute })(AdminProduct)  
